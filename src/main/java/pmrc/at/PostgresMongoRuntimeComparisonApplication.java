dde698684df83de7f693a95625192c9265520e66
Louis Ferro <FER22362@spengergasse.at>
2024-10-31 11:14:04 +0100

    Initialization

---
+++ src/main/java/pmrc/at/PostgresMongoRuntimeComparisonApplication.java
@@ -0,0 +1,13 @@
+package pmrc.at;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresMongoRuntimeComparisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresMongoRuntimeComparisonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pmrc/at/TestPostgresMongoRuntimeComparisonApplication.java
@@ -0,0 +1,11 @@
+package pmrc.at;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestPostgresMongoRuntimeComparisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(PostgresMongoRuntimeComparisonApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
